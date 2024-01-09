ebf9756be9c6b0de696483e3f05b2610ddeaee51
Jason Talge <kontakt@djja.yt>
2024-01-09 14:50:15 +0100

    Added sass in config

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/sass/app.scss',
                 'resources/css/app.css',
                 'resources/js/app.js',
                 'node_modules/jquery/dist/jquery.min.js',
